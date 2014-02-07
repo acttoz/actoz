c7d3d219699679fa14041657cedb5e732ab1f5d8
acttoz <acttoz@a759d4b6-bcf1-0410-8109-97f0a3a4c094>
2014-02-07 10:42:56 +0000

    git-svn-id: https://dev.naver.com/svn/actoz@11 a759d4b6-bcf1-0410-8109-97f0a3a4c094

--- Baedalmal/src/a/com/moon/baedalmal/Intro.java
+++ Baedalmal/src/a/com/moon/baedalmal/Intro.java
@@ -2,33 +2,61 @@ package a.com.moon.baedalmal;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
-import android.view.View;
-import android.view.View.OnClickListener;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 
-public class Intro extends Activity implements OnClickListener {
+public class Intro extends Activity {
+	ImageView logo;
 
 	/** Called when the activity is first created. */
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+		setContentView(R.layout.intro);
+		logo = (ImageView) findViewById(R.id.logo);
 		// TODO Auto-generated method stub
-		ImageView intro = (ImageView) findViewById(R.id.intro);
-		intro.setOnClickListener(this);
+		new Thread(new Runnable() {
+			public void run() {
+
+				final Animation fade_In = AnimationUtils.loadAnimation(
+						Intro.this, R.anim.alpha);
+				final Animation fade_Out = AnimationUtils.loadAnimation(
+						Intro.this, R.anim.fade_out);
+				logo.startAnimation(fade_In);
+				try {
+					Thread.sleep(2500);
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+
+				runOnUiThread(new Runnable() {
+
+					public void run() {
+						// TODO Auto-generated method stub
+						logo.startAnimation(fade_Out);
+						LinearLayout linear = (LinearLayout) findViewById(R.id.linear);
+						linear.setBackgroundColor(Color.rgb(255, 255, 255));
+						logo.setImageResource(R.drawable.moon_logo);
+						logo.startAnimation(fade_In);
+					}
+				});
+
+				try {
+					Thread.sleep(2000);
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+
+				Intent intent2 = new Intent(Intro.this, Main_Study.class);
+				startActivity(intent2);
+				finish();
+			}
+		}).start();
 	}
-
-	@Override
-	public void onClick(View arg0) {
-		// TODO Auto-generated method stub
-//		if (!dbCopied) {
-//			startActivity(new Intent(this, COPYDB.class));
-//		} else {
-			startActivity(new Intent(this, List.class));
-//		}
-		finish();
-
-	}
-
 }
