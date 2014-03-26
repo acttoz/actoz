7ee9d5587b06d606cdcd8bc0526c11e86370a025
acttoz <acttoz@a759d4b6-bcf1-0410-8109-97f0a3a4c094>
2014-03-26 07:26:32 +0000

    git-svn-id: https://dev.naver.com/svn/actoz@58 a759d4b6-bcf1-0410-8109-97f0a3a4c094

--- ##Unity/Assets/src_enemy.cs
+++ ##Unity/Assets/src_enemy.cs
@@ -7,7 +7,7 @@ public class src_enemy : MonoBehaviour
 		// Use this for initialization
 		void Awake ()
 		{
-				rigidbody.velocity = new Vector3 (-5, -5, 0);
+				rigidbody.velocity = new Vector3 (5,-5, 0);
 		}
 
 		void Start ()
@@ -18,6 +18,39 @@ public class src_enemy : MonoBehaviour
 		// Update is called once per frame
 		void Update ()
 		{
+		}
+
+		void OnTriggerEnter (Collider myTrigger)
+		{
+				if (myTrigger.gameObject.name == "down") {
+
+						rigidbody.velocity = new Vector3 (rigidbody.velocity.x, -rigidbody.velocity.y, 0);
+						
+
+						Debug.Log ("wall trigger");
+				}
+
+				if (myTrigger.gameObject.name == "up") {
+			
+						rigidbody.velocity = new Vector3 (rigidbody.velocity.x, -rigidbody.velocity.y, 0);
+			
+			
+						Debug.Log ("wall trigger");
+				}
+				if (myTrigger.gameObject.name == "left") {
+			
+						rigidbody.velocity = new Vector3 (-rigidbody.velocity.x, rigidbody.velocity.y, 0);
+			
+			
+						Debug.Log ("wall trigger");
+				}
+				if (myTrigger.gameObject.name == "right") {
+			
+						rigidbody.velocity = new Vector3 (-rigidbody.velocity.x, rigidbody.velocity.y, 0);
+			
+			
+						Debug.Log ("wall trigger");
+				}
 		}
  
 
