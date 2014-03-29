f134e4f2f8e06ef77f0c51361492c73b209077a4
acttoz <acttoz@a759d4b6-bcf1-0410-8109-97f0a3a4c094>
2014-03-29 15:26:00 +0000

    git-svn-id: https://dev.naver.com/svn/actoz@76 a759d4b6-bcf1-0410-8109-97f0a3a4c094

---
+++ 00_Unity/space_balloon/Assets/scr_toStart.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class scr_toStart : MonoBehaviour
+{
+		public GameObject manager;
+		// Use this for initialization
+		void Start ()
+		{
+	
+		}
+	
+		// Update is called once per frame
+		void Update ()
+		{
+	
+		}
+
+		void OnTap ()
+		{
+				manager.SendMessage ("gameStart");
+		}
+}
---
+++ 00_Unity/space_balloon/Assets/src_balloon.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class src_balloon : MonoBehaviour
+{
+		Animator anim;
+		bool exist = false;
+		public GameObject GAMEMANAGER, pop;
+		public Sprite crash, balloon;
+		// Use this for initialization
+		void Start ()
+		{
+				anim = GetComponent<Animator> ();
+		}
+	
+		// Update is called once per frame
+		void Update ()
+		{
+	
+		}
+
+		void create ()
+		{
+				exist = true;
+				GetComponent<SpriteRenderer> ().sprite = balloon;
+//				Debug.Log ("OnEnable()");
+				anim = GetComponent<Animator> ();
+				anim.SetBool ("balloonExist", true);
+		}
+
+		void cancel (int num)
+		{
+				exist = false;
+				anim.SetBool ("balloonExist", false);
+				anim.SetInteger ("super", 0);
+				anim.SetInteger ("cancel", num);
+				
+		}
+
+		void superMode (int num)
+		{
+				anim.SetInteger ("super", num);
+		
+		}
+		 
+		void OnTriggerEnter (Collider myTrigger)
+		{
+				if (myTrigger.transform.tag == "enemy" && exist) {
+						
+						Debug.Log ("onTrigger");
+						exist = false;
+						GAMEMANAGER.SendMessage ("getBalloonMSG", 1);
+			                        
+						
+				}
+		
+		 
+		}
+
+
+}
