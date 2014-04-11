e739923041884b0bc51d004fa55c39ad1e70992d
acttoz <acttoz@a759d4b6-bcf1-0410-8109-97f0a3a4c094>
2014-04-11 14:24:06 +0000

    git-svn-id: https://dev.naver.com/svn/actoz@122 a759d4b6-bcf1-0410-8109-97f0a3a4c094

---
+++ space_balloon/Assets/scr_item.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class scr_item : MonoBehaviour
+{
+		// Use this for initialization
+		void Start ()
+		{
+		}
+	
+		// Update is called once per frame
+		void Update ()
+		{
+	
+		}
+
+		void OnTriggerEnter (Collider myTrigger)
+		{
+				Destroy (this.gameObject);
+				Debug.Log ("itemGet");
+				if (myTrigger.transform.tag == "balloon") {
+			
+			
+				}
+		
+		
+		}
+
+}
