7ab07575244c04d5ed435a3bb9b5851fa72a50c0
acttoz <acttoz@a759d4b6-bcf1-0410-8109-97f0a3a4c094>
2014-06-10 14:28:28 +0000

    git-svn-id: https://dev.naver.com/svn/actoz@211 a759d4b6-bcf1-0410-8109-97f0a3a4c094

---
+++ Chunk2_3/src/kr/moon/chunk2_3/FileUtil.java
@@ -0,0 +1,30 @@
+/*
+ * Copyright © 2013 Yuk SeungChan, All rights reserved.
+ */
+
+package kr.moon.chunk2_3;
+
+import android.os.Environment;
+
+import java.io.File;
+
+public class FileUtil
+{
+	public String mkdir(String dirName) throws Exception {
+		String rootPath = Environment.getExternalStorageDirectory().getAbsolutePath() + File.separator + dirName;
+		try
+	    {
+			File fileRoot = new File(rootPath);
+			if (fileRoot.exists() == false)
+			{
+				if (fileRoot.mkdirs() == false)
+					throw new Exception("Fail");
+			}
+	    }
+	    catch (Exception e)
+	    {
+            throw new Exception("Fail");
+	    }
+		return rootPath + "/";
+	}
+}
