0d6321728fbdbfc5553ca907c91bd2697214724c
acttoz <acttoz@a759d4b6-bcf1-0410-8109-97f0a3a4c094>
2014-06-16 03:22:12 +0000

    git-svn-id: https://dev.naver.com/svn/actoz@218 a759d4b6-bcf1-0410-8109-97f0a3a4c094

--- Chunk2_2/src/kr/moon/chunk2_2/Custom_List_Data.java
+++
@@ -1,8 +0,0 @@
-package kr.moon.chunk2_2;
-
-public class Custom_List_Data {
-	String Data;
-	int score = 0;
-	int sum = 0;
-
-}
--- Chunk2_3/src/kr/moon/chunk2_3/Custom_List_Data.java
+++
@@ -1,8 +0,0 @@
-package kr.moon.chunk2_3;
-
-public class Custom_List_Data {
-	String Data;
-	int score = 0;
-	int sum = 0;
-
-}
