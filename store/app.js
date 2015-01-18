49c028d332ec50a03a1bda3f66f16cc0f8d61f44
acttoz <acttoz@a759d4b6-bcf1-0410-8109-97f0a3a4c094>
2015-01-18 08:15:05 +0000

    git-svn-id: https://dev.naver.com/svn/actoz@282 a759d4b6-bcf1-0410-8109-97f0a3a4c094

--- store/app.js
+++ store/app.js
@@ -1,15 +1,43 @@
-function getRequest() {
+var app_names = ['손안에서 둘러보는 <br/> 우리나라 특산물', '스마트 사이언스', '전래동화와 함께<br/>떠나는 수학나라', '가야의 땅 경남', '지구와 달', '위인이 들려주는<br/> 역사 이야기', '역사 속 인물탐구', '스마트 실험실', '한국사 타임머신', '사회과부도', '학교생활<br/>퀵 가이드', '무지개 학급경영', '모듬학습<br/>도구 Top 20', '내꺼야?<br/>네꺼야?'];
+var app_ids = ['pe.giulim.specialities1', 'com.moon.recycler', 'com.g11tg.Company.ProductName', 'zz_gimhae.exitschool.kr', 'com.moon.earth.moon', 'com.knowledgeware.modelexecutor.greatman_1410220155', 'com.distoma.personedu', 'air.SmartExperiment', 'kr.go.gne.time.machine.korean.history', 'appinventor.ai_jhujubi.k', 'com.knowledgeware.modelexecutor.teach_helper_1410310126'];
+var appId;
+
+function init() {
+
 	if (location.search.length > 1) {
-		var get = new Object();
-		var ret = location.search.substr(1).split('&');
-		for (var i = 0; i < ret.length; i++) {
-			var r = ret[i].split('=');
-			get[r[0]] = r[1];
-		}
-		return get;
-	} else {
-		return false;
+		appId = location.search.split('id=')[1]
 	}
+
+	$(".icon2").attr('src', 'img/icon/' + appId + '.png');
+	$("#app_content").attr('src', 'img/content/' + appId + '.jpg');
+	$("#app_name").html(app_names[appId - 1]);
+
+	if (appId > 12)
+		$("#app_down").attr('src', 'img/down_pdf.png');
 }
 
-var get = getRequest();
+
+$(".btn_back").click(function(event) {
+	window.location.href = "index.html";
+});
+$("#app_down").click(function(event) {
+	var down_link;
+	if (appId < 12) {
+		down_link = "https://play.google.com/store/apps/details?id=" + app_ids[appId - 1];
+	} else if (appId == 12) {
+		down_link = "https://itunes.apple.com/kr/app/leinbou-haggeubgyeong-yeong/id933807155?mt=8";
+	} else if (appId > 12) {
+		down_link = "pdf/" + appId+".pdf";
+		console.log(down_link)
+	}
+	window.location.href = down_link;
+});
+
+$("#app_ppt").click(function(event) {
+	var down_ppt_link;
+	down_ppt_link = 'ppt/' + appId + '.pptx';
+	window.open(down_ppt_link);
+});
+
+init();
+
--- store/index.js
+++ store/index.js
@@ -2,3 +2,4 @@ $(".icon").click(function(event) {
 	var clickedIcon = event.target.id;
 	window.location.href = "app.html?id=" + clickedIcon;
 });
+ 
