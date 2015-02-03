2ce37c4315775249784f12854d069e559cb03ef6
acttoz <acttoz@a759d4b6-bcf1-0410-8109-97f0a3a4c094>
2015-02-03 03:57:15 +0000

    git-svn-id: https://dev.naver.com/svn/actoz@285 a759d4b6-bcf1-0410-8109-97f0a3a4c094

--- store/app.js
+++ store/app.js
@@ -1,5 +1,5 @@
-var app_names = ['<img src="img/icon/android.png"/>손안에서 둘러보는 <br/> 우리나라 특산물', '<img src="img/icon/android.png"/>스마트 사이언스', '<img src="img/icon/android.png"/>전래동화와 함께<br/>떠나는 수학나라', '<img src="img/icon/android.png"/>가야의 땅 경남', '<img src="img/icon/android.png"/>지구와 달', '<img src="img/icon/android.png"/>위인이 들려주는<br/> 역사 이야기', '<img src="img/icon/android.png"/>역사 속 인물탐구', '<img src="img/icon/android.png"/>스마트 실험실', '<img src="img/icon/android.png"/>한국사 타임머신', '<img src="img/icon/android.png"/>사회과부도', '<img src="img/icon/android.png"/>학교생활<br/>퀵 가이드', '<img src="img/icon/ios.png"/>무지개 학급경영', '모듬학습<br/>도구 Top 20', '내꺼야?<br/>네꺼야?','내 손안의<br/>스마트 2.0'];
-var app_ids = ['pe.giulim.specialities1', 'com.moon.recycler', 'com.g11tg.Company.ProductName', 'zz_gimhae.exitschool.kr', 'com.moon.earth.moon', 'com.knowledgeware.modelexecutor.greatman_1410220155', 'com.distoma.personedu', 'air.SmartExperiment', 'kr.go.gne.time.machine.korean.history', 'appinventor.ai_jhujubi.k', 'com.knowledgeware.modelexecutor.teach_helper_1410310126'];
+var app_names = ['<img class="store_icon" src="img/icon/android.png"/>손안에서 둘러보는 <br/> 우리나라 특산물', '<img class="store_icon" src="img/icon/android.png"/>스마트 사이언스', '<img class="store_icon" src="img/icon/android.png"/>전래동화와 함께<br/>떠나는 수학나라', '<img class="store_icon" src="img/icon/android.png"/>가야의 땅 경남', '<img class="store_icon" src="img/icon/android.png"/>지구와 달', '<img class="store_icon" src="img/icon/android.png"/>위인이 들려주는<br/> 역사 이야기', '<img class="store_icon" src="img/icon/android.png"/>역사 속 인물탐구', '<img class="store_icon" src="img/icon/android.png"/>스마트 실험실', '<img class="store_icon" src="img/icon/android.png"/>한국사 타임머신', '<img class="store_icon" src="img/icon/android.png"/>사회과부도', '<img class="store_icon" src="img/icon/android.png"/>학교생활<br/>퀵 가이드', '<img class="store_icon" src="img/icon/ios.png"/>무지개 학급경영', '<img class="store_icon" src="img/icon/pdf.png"/>모듬학습<br/>도구 Top 21', '<img class="store_icon" src="img/icon/pdf.png"/>내꺼야?<br/>네꺼야?','<img class="store_icon" src="img/icon/pdf.png"/>내 손안의<br/>스마트 2.0','<img class="store_icon" src="img/icon/android.png"/>좋은책'];
+var app_ids = ['pe.giulim.specialities1', 'com.moon.recycler', 'com.g11tg.Company.ProductName', 'zz_gimhae.exitschool.kr', 'com.moon.earth.moon', 'com.knowledgeware.modelexecutor.greatman_1410220155', 'com.distoma.personedu', 'air.SmartExperiment', 'kr.go.gne.time.machine.korean.history', 'appinventor.ai_jhujubi.k', 'com.knowledgeware.modelexecutor.teach_helper_1410310126','kr.go.gne.goodbooks'];
 var appId;
 
 function init() {
@@ -14,6 +14,8 @@ function init() {
 
 	if (appId > 12)
 		$("#app_down").attr('src', 'img/down_pdf.png');
+	if (appId == 16)
+		$("#app_down").attr('src', 'img/down_app.png');
 }
 
 
@@ -26,11 +28,13 @@ $("#app_down").click(function(event) {
 		down_link = "https://play.google.com/store/apps/details?id=" + app_ids[appId - 1];
 	} else if (appId == 12) {
 		down_link = "https://itunes.apple.com/kr/app/leinbou-haggeubgyeong-yeong/id933807155?mt=8";
-	} else if (appId > 12) {
+	} else if (appId > 12 && appId <16) {
 		down_link = "pdf/" + appId+".pdf";
-		console.log(down_link)
+	} else if (appId == 16) {
+		down_link = "https://play.google.com/store/apps/details?id=" + app_ids[11];
 	}
-	window.location.href = down_link;
+	window.open(down_link);
+	// window.location.href = down_link;
 });
 
 $("#app_ppt").click(function(event) {
