c922d89ae1f140925b0d92205c7abcd07d18d96a
acttoz <acttoz@a759d4b6-bcf1-0410-8109-97f0a3a4c094>
2015-06-03 00:40:49 +0000

    git-svn-id: https://dev.naver.com/svn/actoz@305 a759d4b6-bcf1-0410-8109-97f0a3a4c094

---
+++ Alim3/app/src/main/java/kr/co/moon/speedalim/ClassEdit.java
@@ -0,0 +1,18 @@
+package kr.co.moon.speedalim;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+public class ClassEdit extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_class_edit);
+
+    }
+
+
+
+}
