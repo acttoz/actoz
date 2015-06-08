8ad25f44670cec7215b6e564d05901d3f257a2fe
acttoz <acttoz@a759d4b6-bcf1-0410-8109-97f0a3a4c094>
2015-06-08 13:28:13 +0000

    git-svn-id: https://dev.naver.com/svn/actoz@310 a759d4b6-bcf1-0410-8109-97f0a3a4c094

---
+++ Alim3/app/src/main/java/kr/co/moon/speedalim/Account.java
@@ -0,0 +1,18 @@
+package kr.co.moon.speedalim;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class Account extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.account);
+    }
+
+
+}
